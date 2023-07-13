450d55d8a1244b7fdd4a1afda77774e583767c45
Uri Berger <uriber@cs.huji.ac.il>
2023-07-13 14:47:40 +0300

    Fixed bugs in translation training data preperation

--- reformulation_experiment/prepare_translation_training_data.py
+++ reformulation_experiment/prepare_translation_training_data.py
@@ -1,27 +1,28 @@
 import json
+import sys
 import random
 from collections import defaultdict
 
-sample_num = 100
+sample_num = None
 with open('dataset_coco.json', 'r') as fp:
     coco_en_data = json.load(fp)['images']
-data_inds = [i for i in range(len(data)) if data[i]['split'] == 'train']
+data_inds = [i for i in range(len(coco_en_data)) if coco_en_data[i]['split'] == 'train']
 if sample_num is not None:
     data_inds = random.sample(data_inds, sample_num)
 
-with open('coco_de_translated_helskinki.json', 'r') as fp:
+with open('2coco_de_translated_helskinki.json', 'r') as fp:
     coco_de_data = json.load(fp)
 
-caption_inds = [i for i in range(len(data)) for x in data[i]['sentences']]
+caption_inds = [i for i in range(len(coco_en_data)) for x in coco_en_data[i]['sentences']]
 ind_to_captions = defaultdict(list)
-for i in range(len(caption_inds)):
+for i in range(len(coco_de_data)):
     cur_ind = caption_inds[i]
     caption = coco_de_data[i]
-    ind_to_captions[ind].append(caption)
+    ind_to_captions[cur_ind].append(caption)
 
 res = []
 for ind in data_inds:
-    res.append({'image_id': data[ind]['image_id'], 'image_path': f'/cs/labs/oabend/uriber/datasets/COCO/train2014/COCO_train2014_{str(data[ind]["image_id"]).zfill(12)}.jpg', 'sentences': [{'raw': random.choice(ind_to_captions[ind])}]})
+    res.append({'image_id': coco_en_data[ind]['cocoid'], 'image_path': f'/cs/labs/oabend/uriber/datasets/COCO/train2014/COCO_train2014_{str(coco_en_data[ind]["cocoid"]).zfill(12)}.jpg', 'sentences': [{'raw': random.choice(ind_to_captions[ind])}]})
     
 with open(f'reformulation_experiment/data/translated_data/coco_translated_data_{sys.argv[1]}.json', 'w') as fp:
     fp.write(json.dumps(res))
