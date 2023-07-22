39a017ce127d2a95a8e48b3141f88bedfacce0c8
Uri Berger <uriber@cs.huji.ac.il>
2023-07-22 08:21:17 +0300

    Added reformulation english second experiment

---
+++ reformulation_experiment/en/prepare_translation_training_data2.py
@@ -0,0 +1,21 @@
+import json
+import sys
+import random
+from collections import defaultdict
+
+with open('reformulation_experiment/en/data/translated_data/stair_captions_val_en_translated_helsinki.json', 'r') as fp:
+    stair_en_data = json.load(fp)
+
+image_id_to_captions = defaultdict(list)
+for sample in stair_en_data:
+    image_id_to_captions[sample['image_id']].append(sample['caption'])
+
+res = []
+for image_id, captions in image_id_to_captions.items():
+    res.append({'image_id': image_id, 'image_path': f'/cs/labs/oabend/uriber/datasets/COCO/val2014/COCO_val2014_{str(image_id).zfill(12)}.jpg', 'sentences': [{'raw': random.choice(captions)}]})
+    
+with open(f'reformulation_experiment/en/data/translated_data/stair_val_translated_data_{sys.argv[1]}.json', 'w') as fp:
+    fp.write(json.dumps(res))
+
+with open(f'reformulation_experiment/en/data/image_ids/image_ids_2.{sys.argv[1]}.json', 'w') as fp:
+    fp.write(json.dumps([x['image_id'] for x in res]))
---
+++ reformulation_experiment/en/run_experiment2.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+set -e
+
+MSG_PREFIX=[LOG_MSG]
+BASE_DIR=reformulation_experiment/en
+EXP_IND=0
+
+# Translation based training
+echo "$MSG_PREFIX Prepare translation training data"
+venv2/bin/python ${BASE_DIR}/prepare_translation_training_data2.py ${EXP_IND}
+echo "$MSG_PREFIX Translation preprocess"
+venv2/bin/python parse_coco.py --clip_model_type ViT-B/32 --json_file ${BASE_DIR}/data/translated_data/stair_val_translated_data_${EXP_IND}.json --output_file stair_val_translated_data_${EXP_IND}
+echo "$MSG_PREFIX Translation training"
+venv2/bin/python train.py --data ./data/coco/stair_val_translated_data_${EXP_IND}.pkl --out_dir ${BASE_DIR}/output/exp_2.${EXP_IND}_translated --epochs 5 --load_model_from_path ${BASE_DIR}/output/exp_${EXP_IND}_base/coco_prefix-009.pt
+echo "$MSG_PREFIX Translation inference 1 epoch"
+venv2/bin/python inference.py --dataset COCO --model_path ${BASE_DIR}/output/exp_2.${EXP_IND}_translated/coco_prefix-000.pt --split test --output_file ${BASE_DIR}/data/infer/translated2_infer_on_test_${EXP_IND}_1_epoch
+echo "$MSG_PREFIX Translation inference 5 epochs"
+venv2/bin/python inference.py --dataset COCO --model_path ${BASE_DIR}/output/exp_2.${EXP_IND}_translated/coco_prefix-004.pt --split test --output_file ${BASE_DIR}/data/infer/translated2_infer_on_test_${EXP_IND}_5_epoch
+
+# Own captions based training
+echo "$MSG_PREFIX Base inference on val"
+venv2/bin/python inference.py --json_file ${BASE_DIR}/data/image_ids/image_ids_2.${EXP_IND}.json --model_path ${BASE_DIR}/output/exp_${EXP_IND}_base/coco_prefix-009.pt --split val --output_file ${BASE_DIR}/data/infer/base_infer_on_val_${EXP_IND} --dataset COCO
+echo "$MSG_PREFIX Own data preperation"
+venv2/bin/python ${BASE_DIR}/prepare_own_training_data.py ${EXP_IND}
+echo "$MSG_PREFIX Own captions preprocess"
+venv2/bin/python parse_coco.py --clip_model_type ViT-B/32 --json_file ${BASE_DIR}/data/own_train_data/own_train_data_${EXP_IND}.json --output_file coco_val_generated_data_${EXP_IND}
+echo "$MSG_PREFIX Own captions training"
+venv2/bin/python train.py --data ./data/coco/coco_val_generated_data_${EXP_IND}.pkl --out_dir ${BASE_DIR}/output/exp_2.${EXP_IND}_own --epochs 5 --load_model_from_path ${BASE_DIR}/output/exp_${EXP_IND}_base/coco_prefix-009.pt
+echo "$MSG_PREFIX Own captions inference 1 epoch"
+venv2/bin/python inference.py --dataset COCO --model_path ${BASE_DIR}/output/exp_2.${EXP_IND}_own/coco_prefix-000.pt --split test --output_file ${BASE_DIR}/data/infer/own_infer_on_test_${EXP_IND}_1_epoch
+echo "$MSG_PREFIX Own captions inference 5 epochs"
+venv2/bin/python inference.py --dataset COCO --model_path ${BASE_DIR}/output/exp_2.${EXP_IND}_own/coco_prefix-004.pt --split test --output_file ${BASE_DIR}/data/infer/own_infer_on_test_${EXP_IND}_5_epoch
+
+# Reformulations based training
+cd ../AliceMind/mPLUG
+echo "$MSG_PREFIX Reformulation"
+venv/bin/python reformulate.py --model_path output/vqa_mplug_base/checkpoint_07.pth --input_file ../../CLIP_prefix_caption/${BASE_DIR}/data/infer/base_infer_on_val_${EXP_IND}.json --split val --output_format caption --output_file ../../CLIP_prefix_caption/${BASE_DIR}/data/infer/base_infer_on_val_${EXP_IND}_reformulated --dataset COCO
+cd ../../CLIP_prefix_caption
+echo "$MSG_PREFIX Reformulations data preperation"
+venv2/bin/python ${BASE_DIR}/prepare_reformulation_training_data2.py ${EXP_IND}
+echo "$MSG_PREFIX Reformulations preprocess"
+venv2/bin/python parse_coco.py --clip_model_type ViT-B/32 --json_file ${BASE_DIR}/data/re_train_data/reformulations_train_data_2.${EXP_IND}.json --output_file coco_val_reformulated_data_${EXP_IND}
+echo "$MSG_PREFIX Reformulations training"
+venv2/bin/python train.py --data ./data/coco/coco_val_reformulated_data_${EXP_IND}.pkl --out_dir ${BASE_DIR}/output/exp_2.${EXP_IND}_reformulated --epochs 5 --load_model_from_path ${BASE_DIR}/output/exp_${EXP_IND}_base/coco_prefix-009.pt
+echo "$MSG_PREFIX Reformulations inference 1 epoch"
+venv2/bin/python inference.py --dataset COCO --model_path ${BASE_DIR}/output/exp_2.${EXP_IND}_reformulated/coco_prefix-000.pt --split test --output_file ${BASE_DIR}/data/infer/reformulations_infer_on_test_2.${EXP_IND}_1_epoch.json
+echo "$MSG_PREFIX Reformulations inference 5 epochs"
+venv2/bin/python inference.py --dataset COCO --model_path ${BASE_DIR}/output/exp_2.${EXP_IND}_reformulated/coco_prefix-004.pt --split test --output_file ${BASE_DIR}/data/infer/reformulations_infer_on_test_2.${EXP_IND}_5_epoch.json
