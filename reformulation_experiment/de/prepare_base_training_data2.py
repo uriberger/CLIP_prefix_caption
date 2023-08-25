8323f5e8f190264d7bfd1affe86fe5191c06ed5e
Uri Berger <uriber@cs.huji.ac.il>
2023-08-25 23:42:13 +0300

    Fixed typos

--- reformulation_experiment/de/prepare_base_training_data2.py
+++ reformulation_experiment/de/prepare_base_training_data2.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 from utils import get_caption_data_for_split
 import pickle
 
-assert len(sys.argv) == 3
+assert len(sys.argv) == 2
 exp_ind = int(sys.argv[1])
 
 with open('/cs/labs/oabend/uriber/datasets/flickr30/karpathy/dataset_flickr30k.json', 'r') as fp:
@@ -32,7 +32,7 @@ for x in coco_data:
     coco_orig_to_new_image_ids[x['cocoid']] = len(new_to_orig_image_ids)
     new_to_orig_image_ids.append(('COCO', x['cocoid']))
 
-all_aic_images_ids = []
+all_aic_image_ids = []
 for x in aic_train_data + aic_val_data:
     image_id = int(x['image_id'].split('.jpg')[0], 16)
     aic_orig_to_new_image_ids[image_id] = len(new_to_orig_image_ids)
@@ -54,11 +54,11 @@ with open(f'reformulation_experiment/de/data/image_ids/coco_image_ids_{exp_ind}.
 
 multi30k_data = get_caption_data_for_split('train') + get_caption_data_for_split('val') + get_caption_data_for_split('test_2016')
 train_image_ids = [int(x['filename'].split('.jpg')[0]) for x in flickr_data if x['split'] != 'test']
-image_ids_dict = {x: True for x in chosen_image_ids}
+image_ids_dict = {x: True for x in train_image_ids}
 train_data = [x for x in multi30k_data if x['image_id'] in image_ids_dict]
 
 image_id_to_captions = defaultdict(list)
-for x in cur_train_data:
+for x in train_data:
     image_id_to_captions[x['image_id']].append(x['caption'])
 
 res = [{'image_id': x[0], 'image_path': f'/cs/labs/oabend/uriber/datasets/flickr30/images/{x[0]}.jpg', 'sentences': [{'raw': y} for y in x[1]]} for x in image_id_to_captions.items()]
