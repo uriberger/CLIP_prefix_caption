b8591060b60424a05c029a0aa0802f92a5b71a65
Uri Berger <uriber@cs.huji.ac.il>
2023-09-07 13:57:30 +0300

    Some bug fixes in data preperation

--- reformulation_experiment/de/prepare_reformulation_training_data_coco_aic.py
+++ reformulation_experiment/de/prepare_reformulation_training_data_coco_aic.py
@@ -10,10 +10,10 @@ with open(f'reformulation_experiment/de/data/infer/base_infer_on_aic_{sys.argv[1
 
 coco_image_id_to_split = {}
 with open('dataset_coco.json', 'r') as fp:
-    coco_data = json.load(fp)['images']
-    for sample in coco_data:
+    coco_orig_data = json.load(fp)['images']
+    for sample in coco_orig_data:
         image_id = sample['cocoid']
-        image_id_to_split[image_id] = sample['filepath'].split('2014')[0]
+        coco_image_id_to_split[image_id] = sample['filepath'].split('2014')[0]
 
 aic_image_id_to_split = {}
 with open('/cs/labs/oabend/uriber/datasets/ai_challenger/ai_challenger_caption_train_20170902/caption_train_annotations_20170902.json', 'r') as fp:
@@ -21,11 +21,11 @@ with open('/cs/labs/oabend/uriber/datasets/ai_challenger/ai_challenger_caption_t
 with open('/cs/labs/oabend/uriber/datasets/ai_challenger/ai_challenger_caption_validation_20170910/caption_validation_annotations_20170910.json', 'r') as fp:
     aic_val_data = json.load(fp)
 for sample in aic_train_data:
-    image_id = int(x['image_id'].split('.jpg')[0], 16)
-    image_id_to_split[image_id] = 'train'
+    image_id = int(sample['image_id'].split('.jpg')[0], 16)
+    aic_image_id_to_split[image_id] = 'train'
 for sample in aic_val_data:
-    image_id = int(x['image_id'].split('.jpg')[0], 16)
-    image_id_to_split[image_id] = 'validation'
+    image_id = int(sample['image_id'].split('.jpg')[0], 16)
+    aic_image_id_to_split[image_id] = 'validation'
 split_to_date = {'train': '20170902', 'validation': '20170910'}
     
 res = []
@@ -37,7 +37,7 @@ for sample in aic_data:
     image_id = sample['image_id']
     split = aic_image_id_to_split[image_id]
     date = split_to_date[split]
-    file_name = hex(image_id)[2:].zfill(40)
+    file_name = hex(image_id)[2:].zfill(40) + '.jpg'
     res.append({'image_id': image_id, 'image_path': f'/cs/labs/oabend/uriber/datasets/ai_challenger/ai_challenger_caption_{split}_{date}/caption_{split}_images_{date}/{file_name}', 'sentences': [{'raw': sample['caption']}]})
     
 with open(f'reformulation_experiment/de/data/re_train_data/reformulations_train_data_coco_aic_{sys.argv[1]}.json', 'w') as fp:
